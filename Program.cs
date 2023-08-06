8231e4428db2232a36cf3bb5d1441ea4cd9a65d3
Tomas Dambrauskas <tomas.sql@protonmail.com>
2023-08-07 00:18:29 +0300

    the proof.

--- Program.cs
+++ Program.cs
@@ -1,20 +1,46 @@
-﻿internal class Program {
-    private static void Main(string[] args) {
-        var sum = 0;
-        var tasks = new List<Task>();
+﻿var sum = 0;
+var threads = new List<Thread>(1000);
+var cts = new CancellationTokenSource();
+var rng = new Random();
 
-        for (int i = 0; i < 1000; i++) {
-            var t = Task.Factory.StartNew(async () => {
-                for (int j = 0; j < 1000; j++) {
-                    using var _ = await (await typeof(Marker).GetLockAsync()).LockAsync().ConfigureAwait(false);
-                    sum++;
-                }
-            });
-            tasks.Add(t);
-        }
-        Task.WaitAll(tasks.ToArray());
-        
-        Console.WriteLine(sum);
+async Task do_it_1() {
+    for (int j = 0; j < 1000; j++) {
+        await Task.Delay(rng.Next(2));
+        sum++;
     }
-
 }
+
+async Task do_it_2() {
+    for (int j = 0; j < 1000; j++) {
+        await Task.Delay(rng.Next(2));
+        using var _ = await typeof(Marker)
+            .GetLock()
+            .LockAsync(cts.Token)
+            .ConfigureAwait(false);
+        sum++;
+    }
+}
+
+for (int i = 0; i < 1000; i++) {
+    var t = new Thread(() => do_it_1().GetAwaiter().GetResult());
+    threads.Add(t);
+    t.Start();
+};
+
+Console.WriteLine("Lets wait fot do it 1!");
+threads.AsParallel().ForAll(t => t.Join());
+threads.Clear();
+Console.WriteLine(sum);
+sum = 0;
+
+for (int i = 0; i < 1000; i++) {
+    var t = new Thread(() => do_it_2().GetAwaiter().GetResult());
+    threads.Add(t);
+    t.Start();
+};
+
+Console.WriteLine("Lets wait fot do it 2!");
+threads.AsParallel().ForAll(t => t.Join());
+threads.Clear();
+Console.WriteLine(sum);
+sum = 0;
